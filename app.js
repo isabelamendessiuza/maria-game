a2184314c5530da4c152e82eee1b1de7f352af42
isabelamendessiuza <131874069+isabelamendessiuza@users.noreply.github.com>
2024-08-26 16:08:09 -0300

    h

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
-alert('Boas-vindas ao jogo do Número Secreto');
 
-let numeroSecreto = 29;
-prompt('Escolha um número entre 1 e 30');
+
+
+while
+(chute != numeroSecreto) {
+
+
+ 
+  chute = prompt('Escolha um número entre 1 e 10');
+
+
+ 
+  // se chute for igual ao número secreto
+
+
+ 
+  if (chute == numeroSecreto) {
+
+
+ 
+      alert(`Isso aí! Você descobriu o número secreto
+${numeroSecreto}`);
+
+
+ 
+
+
+ 
+  } else {
+
+
+ 
+      if (chute > numeroSecreto) {
+
+
+ 
+      alert (`O numeroSecreto é menor que ${chute}`);
+
+
+ 
+      } else {
+
+
+ 
+      alert (`O numeroSecreto é maior que ${chute}`);
+
+
+ 
+      }
+
+
+ 
+  }
+
+
+}
